1fcf9407301bcff6e8135ddceefa0dc654119d45
Francesco <francescocamporeale23@gmail.com>
2026-04-14 11:39:07 +0200

    fix: firebase config file now exports the db

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANEVPSke2bWdCGY-zbGne6mGUIpgZtWAA",
+  authDomain: "scanos-76346.firebaseapp.com",
+  projectId: "scanos-76346",
+  storageBucket: "scanos-76346.firebasestorage.app",
+  messagingSenderId: "804722044562",
+  appId: "1:804722044562:web:3aa37fe15f4f679f5ec04b",
+  measurementId: "G-0L5FD0HPZS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+
